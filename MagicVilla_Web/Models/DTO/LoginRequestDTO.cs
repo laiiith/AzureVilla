5f345a9fdaa4e77984b15cb2fe321da4af139fec
Laith <Pikachu.jo02@gmail.com>
2024-03-12 09:36:59 +0300

    Fix The Validations

--- MagicVilla_Web/Models/DTO/LoginRequestDTO.cs
+++ MagicVilla_Web/Models/DTO/LoginRequestDTO.cs
@@ -1,9 +1,14 @@
-﻿namespace MagicVilla_Web.Models.DTO
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Web.Models.DTO
 { 
     public class LoginRequestDTO
     {
+        [Required]
+        [DisplayName("Email")]
         public string UserName { get; set; }
-
+        [Required]
         public string Password { get; set; }
     }
 }
--- MagicVilla_Web/Models/DTO/RegisterationRequestDTO.cs
+++ MagicVilla_Web/Models/DTO/RegisterationRequestDTO.cs
@@ -1,10 +1,18 @@
-﻿namespace MagicVilla_Web.Models.DTO
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Web.Models.DTO
 {
     public class RegisterationRequestDTO
     {
+        [Required]
+        [DisplayName("Email")]
         public string UserName { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string Role { get; set; }
     }
 }
